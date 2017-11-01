4ecc07d0bc755480cbb60fcdd037f12c8c8acaf4
sk811613 <sk811613@COB-LABBA>
2017-10-31 20:30:13 -0400

    Data annotations on register page

--- MIS4200Group6/Models/userDetails.cs
+++ MIS4200Group6/Models/userDetails.cs
@@ -16,31 +16,38 @@ namespace MIS4200Group6.Models
         [Required]
         public Guid ID { get; set; }
 
-        [Required]
-        [EmailAddress]
         [Display(Name = "Email")]
+        [Required]
+        [EmailAddress(ErrorMessage = "Enter your most frequently used email address")]
         public string email { get; set; }
 
-        [Required]
         [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Employee first name is required")]
+        [StringLength(20)]
         public string firstName { get; set; }
 
-        [Required]
         [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Employee last name is required")]
+        [StringLength(20)]
         public string lastName { get; set; }
 
-        [Phone]
-        [Display(Name = "Primary Phone")]
+        [Display(Name = "Mobile phone number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\(d{3}\) |\d{3}-)\d{3}-\d{4}$",
+            ErrorMessage = "Phone numbers must be in the format (xxx) xxx-xxxx or xxx-xxx-xxxx")]
         public string phoneNumber { get; set; }
 
         [Display(Name = "Business Unit")]
+        [Required(ErrorMessage = "Employee location is required")]
         public string office { get; set; }
 
         [Display(Name = "Current Position")]
+        [Required(ErrorMessage = "Employee position is required")]
         public string Position { get; set; }
 
         [Display(Name = "Centric Anniversary")]
-        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public string HireDate { get; set; }
 
         public string photo { get; set; }
