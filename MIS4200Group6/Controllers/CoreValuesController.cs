6709d6f2d5a4159dae5a03792eae6b78b69d99e6
km004013 <km004013@ohio.edu>
2017-11-15 10:34:18 -0500

    Added description of control text field for users

--- MIS4200Group6/Controllers/CoreValuesController.cs
+++ MIS4200Group6/Controllers/CoreValuesController.cs
@@ -49,7 +49,7 @@ namespace MIS4200Group6.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,award,recognizer,recognized,recognizationDate")] CoreValue coreValue)
+        public ActionResult Create([Bind(Include = "ID,award,description,recognizer,recognized,recognizationDate")] CoreValue coreValue)
         {
             if (ModelState.IsValid)
             {
@@ -90,7 +90,7 @@ namespace MIS4200Group6.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,award,recognizer,recognized,recognizationDate")] CoreValue coreValue)
+        public ActionResult Edit([Bind(Include = "ID,award,description,recognizer,recognized,recognizationDate")] CoreValue coreValue)
         {
             if (ModelState.IsValid)
             {
--- MIS4200Group6/Models/CoreValue.cs
+++ MIS4200Group6/Models/CoreValue.cs
@@ -8,13 +8,20 @@ namespace MIS4200Group6.Models
 {
     public class CoreValue
     {
-        public int ID {get; set;} 
+        public int ID {get; set;}
+
         [Display(Name ="Core Value Recognized")]
         public CoreValues award { get; set; }
-        [Display(Name ="Person Giving the Recognition")]
+        
+        [Display(Name = "Description of Core Value")]
+        public string description { get; set; }
+
+        [Display(Name = "Person Giving the Recognition")]
         public Guid recognizer { get; set; }
+
         [Display(Name ="Person Receiving the Recognition")]
         public Guid recognized { get; set; }
+
         [Display(Name = "Date of Recognition")]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime recognizationDate { get; set; }
