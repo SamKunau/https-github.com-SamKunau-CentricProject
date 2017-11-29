af916fb40809332379e5c217751752208e50341f
Nathan Kocan <nk935513@ohio.edu>
2017-11-29 13:52:13 -0500

    User recognition works

--- MIS4200Group6/DAL/Context.cs
+++ MIS4200Group6/DAL/Context.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 using MIS4200Group6.Models;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace MIS4200Group6.DAL
 {
@@ -17,5 +18,9 @@ namespace MIS4200Group6.DAL
         public DbSet<userDetails> UserDetails { get; set; }
         public DbSet<Recog2> Recog2 { get; set; }
         public System.Data.Entity.DbSet<MIS4200Group6.Models.CoreValue> CoreValues { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
     }
 }
